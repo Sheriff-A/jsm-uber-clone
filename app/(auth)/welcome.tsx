7245c5b80cf7419924e35b409aef66abbbdb3008
Sheriff-A <manshari619@yahoo.com>
2025-01-26 20:00:45 -0500

    project clean up and structure setup

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Welcome = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-3xl">Onboarding</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
